a33a0855d17a3287ed0da6535b7b15fe72ea7444
maxibra <max.braver@gmail.com>
2022-07-02 11:31:52 +0300

    update fibonacii logic

--- app/Fibonacii.py
+++ app/Fibonacii.py
@@ -1,5 +1,7 @@
 import sys
+import click
 
+from random import randint
 from time import sleep
 
 
@@ -12,17 +14,32 @@ def fibonacii(n: int) -> int:
     return seq
 
 
-def print_fib_seq(fib_seq: list, sleep_time: int) -> None:
-    while fib_seq:
-        print(fib_seq.pop(0), flush=True)
-        if fib_seq:
-            sleep(sleep_time)
+def print_fib(max_random: int, sleep_time: int) -> None:
+    while True:
+        print(fibonacii(randint(1, max_random)), flush=True)
+        sleep(sleep_time)
+
+
+@click.command()
+@click.option(
+    "-l",
+    "--seq_len",
+    default=10,
+    show_default=True,
+    type=int,
+    help="The maximal length of the Fibonacci sequence.",
+)
+@click.option(
+    "-s",
+    "--sleep",
+    default=60,
+    show_default=True,
+    type=int,
+    help="The sleep time between printing next sequence.",
+)
+def main(seq_len: int, sleep: int) -> None:
+    print_fib(seq_len, sleep)
 
 
 if __name__ == "__main__":
-    f_number = int(sys.argv[1])
-    sleep_time = 60
-    if len(sys.argv) == 3:
-        sleep_time = int(sys.argv[2])
-
-    print_fib_seq(fibonacii(f_number), sleep_time)
+    main()
