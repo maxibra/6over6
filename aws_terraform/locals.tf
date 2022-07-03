cfbb4ae9c59939ef7a6204e825389222c065ff7a
maxibra <max.braver@gmail.com>
2022-07-03 07:08:32 +0300

    add foreach

--- aws_terraform/locals.tf
+++ aws_terraform/locals.tf
@@ -1,6 +1,14 @@
 locals {
-  app_name   = "fibonacii"
-  ecs_cpu    = 512
-  ecs_memory = 1024
-  docker_tag = "latest"
+  ecs_tasks = {
+    fibonacii = {
+        ecs_cpu    = 512
+        ecs_memory = 1024
+        docker_tag = "latest"
+    }
+    jenkins = {
+        ecs_cpu    = 1024
+        ecs_memory = 2048
+        docker_tag = "latest"
+    }
+  }
 }
